717fe805b6f41053471083856459fe578e0f8a00
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-05-30 18:39:42 +0000

    Upload Products data and start to create products page. Added background image to base.html

--- products/views.py
+++ products/views.py
@@ -2,7 +2,17 @@
 Products Views
 """
 from django.shortcuts import render
+from .models import Product
 
 
 # Create your views here.
-    
+def products_list(request):
+    """
+    The Products List view displays the list of products, as well as
+    sorting and search queries
+    """
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
--- wagging_tailor/settings.py
+++ wagging_tailor/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-MEDIIA_URL = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
