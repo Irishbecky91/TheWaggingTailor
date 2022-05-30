4c88f5851f5be21049af96011f343459197606a6
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-05-30 15:32:34 +0000

    Add to Products Admin for list_display and ordering

--- products/admin.py
+++ products/admin.py
@@ -6,5 +6,32 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Defines how the Product is used and displayed in Admin
+    """
+    list_display = (
+        'product_name',
+        'category',
+        'description',
+        'price',
+        'rating',
+        'has_sizes',
+        'image',
+    )
+
+    ordering = ('product_name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Defines how the Category is used and displayed in Admin
+    """
+    list_display = (
+        'category_name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
