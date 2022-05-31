f5fb0f05477c8315fd9c5132788a825a9e9424fc
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-05-31 22:17:20 +0000

    Adjusted styling on home page, created product info html and views, and updated links to product_info

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.products_list, name='products'),
+    path('<product_id>', views.product_info, name='product_info'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 """
 Products Views
 """
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -16,3 +16,15 @@ def products_list(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_info(request, product_id):
+    """
+    The Product Info view displays the information for individual products.
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_info.html', context)
