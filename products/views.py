d60b2b3d8b6491063214b00e204f3accb1ab5d69
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-06-01 21:25:11 +0000

    Added sorting functionality to navbar for price, rating and category

--- products/views.py
+++ products/views.py
@@ -16,8 +16,23 @@ def products_list(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort'in request.GET:
+            sort_key = request.GET['sort']
+            sort = sort_key
+            if sort_key == 'product_name':
+                sort_key = 'lower_name'
+                products = products.annotate(lower_name=Lower('product_name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sort_key = f'-{sort_key}'
+            products = products.order_by(sort_key)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__category_name__in=categories)
@@ -31,10 +46,13 @@ def products_list(request):
             queries = Q(product_name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
     return render(request, 'products/products.html', context)
 
