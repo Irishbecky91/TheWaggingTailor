9acc98f3cff224e43c2a312bdead9a3def46e78e
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-06-05 20:19:54 +0000

    Corrected typo in settings.py causing build to fail in Heroku

--- wagging_tailor/settings.py
+++ wagging_tailor/settings.py
@@ -170,7 +170,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # static and media files
-    STATICFILES_STORAGE = 'custom_storage.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
