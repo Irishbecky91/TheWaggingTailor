61d251c01e2460b1752a5ef9bade72550c67da9a
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-06-11 21:25:20 +0000

    Updated Country with django countries CountryField dropdown instead of CharField textbox

--- checkout/models.py
+++ checkout/models.py
@@ -6,6 +6,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from django_countries.fields import CountryField
 
 from products.models import Product
 
@@ -25,7 +26,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     shipping_cost = models.DecimalField(max_digits=6, decimal_places=2,
                                         null=False, default=0)
