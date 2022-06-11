1ab5bb0c36b96842f1533fe1a29b70f0a66b91d9
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-06-11 18:00:04 +0000

    Added loading spinner overlay to checkout page

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -29,6 +29,7 @@ var card = elements.create('card', {
 });
 card.mount('#card-element');
 
+
 // Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
@@ -45,6 +46,7 @@ card.addEventListener('change', function (event) {
     }
 });
 
+
 // Handle form submit
 var form = document.getElementById('payment-form');
 
@@ -53,7 +55,9 @@ form.addEventListener('submit', function (ev) {
     card.update({
         'disabled': true
     });
-    $('#submit_button').attr('disabled', true)
+    $('#submit-button').attr('disabled', true)
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -67,8 +71,10 @@ form.addEventListener('submit', function (ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({'disabled': false});
-            $('#submit_button').attr('disabled', false)
+            $('#submit-button').attr('disabled', false)
         } else {
             // The payment has been processed!
             if (result.paymentIntent.status === 'succeeded') {
