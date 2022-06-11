b73ee633938cd03aa5e58e8497b406ee570d12d6
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-06-11 20:59:31 +0000

    Removed webhook related code as this will not be included in the final project

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,11 +3,12 @@ Checkout URLs
 """
 from django.urls import path
 from . import views
-from .webhooks import webhook
+
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_successful/<order_number>', views.checkout_successful,
          name='checkout_successful'),
-    path('wh/', webhook, name='webhook'),
+    path('cache_checkout_data/', views.cache_checkout_data,
+         name='cache_checkout_data'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,13 @@
 """
 Checkout Views
 """
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, \
+    get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 import stripe
+import json
 
 from shopping_bag.contexts import bag_contents
 from products.models import Product
@@ -13,6 +16,23 @@ from .forms import OrderForm
 
 
 # Create your views here.
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, "We're sorry, your payment cannot be \
+            processed right now. Please try again later.")
+        return HttpResponse(content=e, status=400)
+
+
 def checkout(request):
     """
     Renders the secure checkout page where user's details can
