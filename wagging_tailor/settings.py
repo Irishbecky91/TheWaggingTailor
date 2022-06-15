8782d6b72b6ebd67f8dc0d8fd27b1edbe7974713
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-06-15 18:54:20 +0000

    Changed setting to test on Heroku as development 8000 port can access checkout but depolyed site cannot

--- wagging_tailor/settings.py
+++ wagging_tailor/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['the-wagging-tailor.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['the-wagging-tailor.herokuapp.com/checkout/', 'localhost']
 
 
 # Application definition
