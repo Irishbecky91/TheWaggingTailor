6e29685869cc517a395ec3a492f284b02d7838b0
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-06-18 13:48:35 +0000

    Set debug to True to find cause of the 500 error when registering for account on deployed site

--- wagging_tailor/settings.py
+++ wagging_tailor/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True  # 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-wagging-tailor.herokuapp.com', 'localhost']
 
