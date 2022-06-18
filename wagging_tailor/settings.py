ec48f1220bd6f5df728afcf59e4cacd1d9e51ef5
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-06-18 18:22:00 +0000

    Changed some settings and config variables while troubleshooting email bug

--- wagging_tailor/settings.py
+++ wagging_tailor/settings.py
@@ -214,15 +214,14 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'thewaggingtailor.store@example.com'
+    DEFAULT_FROM_EMAIL = 'thewaggingtailor.store@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_USE_SSL = False
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = os.environ.get('EMAIL_PORT')
+    EMAIL_HOST = os.environ.get('EMAIL_HOST')
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
-print(os.environ.get("EMAIL_HOST_PASS"))
+print(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
