4c04e1ca7c9369eb575f30f991b443deae9221e1
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-06-18 18:48:14 +0000

    Changed some content of allauth emails to show wagging tailor instead of example email

--- wagging_tailor/settings.py
+++ wagging_tailor/settings.py
@@ -223,5 +223,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-print(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
